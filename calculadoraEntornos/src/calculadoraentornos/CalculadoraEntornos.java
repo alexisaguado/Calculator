b6dfe0759b7402f03a91ab6c20061f28269f43d1
alexisaguado <alexis.garagu@educa.jcyl.es>
2024-02-23 19:00:18 +0100

    added mostrarMenu() method
    Método para mostrar el menú de opciones de la calculadora

--- calculadoraEntornos/src/calculadoraentornos/CalculadoraEntornos.java
+++ calculadoraEntornos/src/calculadoraentornos/CalculadoraEntornos.java
@@ -43,8 +43,21 @@ public class CalculadoraEntornos {
         }
 
         sc.close();
-        
-        
     }
     
+   /*
+    * Muestra un menú simple de opciones para una calculadora en Java.
+    * Imprime en la consola las opciones disponibles, solicitando al usuario que seleccione una opción.
+    * 
+    * Este método no acepta parámetros y no devuelve ningún valor.
+    */
+     private static void mostrarMenu() {
+        System.out.println("Bienvenidos a nuestra calculadora");
+        System.out.println("1. Sumar");
+        System.out.println("2. Restar");
+        System.out.println("3. Multiplicar");
+        System.out.println("4. Dividir");
+        System.out.println("5. Salir");
+        System.out.print("Seleccione una opción (1-5): ");
+    }
 }
